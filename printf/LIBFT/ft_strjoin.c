7652bc920fcc4828e11f7a0922a5ada2d5664938
jduraes- <jdbarros99@gmail.com>
2023-05-15 00:43:52 +0100

    addig

---
+++ printf/LIBFT/ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jduraes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 16:10:21 by jduraes-          #+#    #+#             */
+/*   Updated: 2023/05/04 16:20:32 by jduraes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		ii;
+	char	*str;
+
+	i = 0;
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	ii = 0;
+	while (s2[ii])
+	{
+		str[i] = s2[ii];
+		i++;
+		ii++;
+	}
+	str[i] = '\0';
+	return (str);
+}
